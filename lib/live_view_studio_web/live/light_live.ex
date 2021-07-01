8520fb7f82b58c0b8d0cf56951859c9460d119a8
Jhonatan Martins <jhonatanbmartins@gmail.com>
2021-07-01 17:10:02 -0300

    light color

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule  LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 10)
+    socket = assign(socket, brightness: 10, temp: 3000)
     {:ok, socket}
   end
 
@@ -10,11 +10,12 @@ defmodule  LiveViewStudioWeb.LightLive do
     ~L"""
       <h1>Front Porch Light</h1>
       <div id="light">
-        <div class="meter">
-          <span style="width: <%= @brightness %>%">
-            <%= @brightness %>%
-          </span>
-        </div>
+      <div class="meter">
+        <span style="background-color: <%= temp_color(@temp) %>;
+                    width: <%= @brightness %>%">
+          <%= @brightness %>%
+        </span>
+      </div>
 
         <button phx-click="off">
           <img src="images/light-off.svg">
@@ -38,6 +39,18 @@ defmodule  LiveViewStudioWeb.LightLive do
 
         <form phx-change="update">
             <input type="range" min="1" max="100" name="brightness" value="<%= @brightness %>" />
+
+            <input type="radio" id="3000" name="temp" value="3000"
+              <%= if 3000 == @temp, do: "checked" %> />
+            <label for="3000">3000</label>
+
+            <input type="radio" id="4000" name="temp" value="4000"
+                <%= if 4000 == @temp, do: "checked" %> />
+            <label for="4000">4000</label>
+
+            <input type="radio"id="5000" name="temp" value="5000"
+                <%= if 5000 == @temp, do: "checked" %> />
+            <label for="5000">5000</label>
         </form>
       </div>
     """
@@ -68,9 +81,14 @@ defmodule  LiveViewStudioWeb.LightLive do
     {:noreply, socket}
   end
 
-  def handle_event("update", %{"brightness" => brightness}, socket) do
+  def handle_event("update", %{"brightness" => brightness, "temp" => temp}, socket) do
     brightness = String.to_integer(brightness)
-    socket = assign(socket, brightness: brightness)
+    temp = String.to_integer(temp)
+    socket = assign(socket, brightness: brightness, temp: temp)
     {:noreply, socket}
   end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
 end
